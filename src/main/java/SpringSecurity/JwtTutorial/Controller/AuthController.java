68777cbf7c97b1356b2ce5abf8e3ee328535256c
jwPark <1211abc@naver.com>
2022-10-05 19:52:45 +0900

    feat: Auth, Controller 구현

---
+++ src/main/java/SpringSecurity/JwtTutorial/Controller/AuthController.java
@@ -0,0 +1,35 @@
+package SpringSecurity.JwtTutorial.Controller;
+
+import SpringSecurity.JwtTutorial.Controller.Dto.MemberRequestDto;
+import SpringSecurity.JwtTutorial.Controller.Dto.MemberResponseDto;
+import SpringSecurity.JwtTutorial.Controller.Dto.TokenDto;
+import SpringSecurity.JwtTutorial.Controller.Dto.TokenRequestDto;
+import SpringSecurity.JwtTutorial.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
+}
